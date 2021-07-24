cc8f9150ef97abbebd4532f9afebd67d27bc7d80
Zuko Mgwili <zukomgwili@gmail.com>
2021-07-24 19:43:13 +0200

    refactor: simplify the editor component

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,30 +1,41 @@
-import React, {useState} from "react";
-import EditorLine from "./EditorLine";
-import {ILine} from "./types";
+import React, {useEffect, useState} from "react";
 
 const Editor: React.FC = () => {
-    const [lines, setLines] = useState<ILine[]>([
-        {
-            lineNumber: 1,
-            lineText: "Hello World!",
-        },
-    ]);
+    const [text, setText] = useState<string>("");
+    const [lineNumbers, setLineNumbers] = useState<number[]>([]);
 
-    const handleEnterKey = () => {
-        setLines([...lines, {lineNumber: lines.length + 1, lineText: ""}]);
+    const getLineNumbers = (value: string) => {
+        return value.split("\n").filter((line) => line.trim() !== "").map((line, index) => index + 1);
+    };
+
+    useEffect(() => {
+        if (text.trim().length === 0) {
+            return;
+        }
+        setLineNumbers(getLineNumbers(text));
+    }, [text]);
+
+    const handleChange = (event: React.ChangeEvent<HTMLDivElement>) => {
+        setText(event.target.innerText as string);
     };
 
     return (
         <div
             style={{
                 border: "1px solid #CCC",
+                display: "flex",
+                flexDirection: "row",
                 height: "100vh",
                 width: "100vw",
             }}
         >
-            {lines.map((line) => (
-                <EditorLine line={line} onEnterKey={handleEnterKey}/>
-            ))}
+            <div style={{
+                flex: "10%",
+            }}>{lineNumbers.map((lineNumber, index) => (<div key={index}>{lineNumber}</div>))}</div>
+            <div style={{
+                flex: "90%",
+            }} contentEditable onInput={handleChange}>
+            </div>
         </div>
     );
 };
